6fff697fe8ce234c6b41ca9c36eb47fa4415b340
H33Kz <qlmejk@gmail.com>
2023-10-29 08:19:39 +0100

    Initial commit

---
+++ src/main/java/com/h33kz/WarehouseManagementSystem/WarehouseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.h33kz.WarehouseManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/h33kz/WarehouseManagementSystem/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h33kz.WarehouseManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
