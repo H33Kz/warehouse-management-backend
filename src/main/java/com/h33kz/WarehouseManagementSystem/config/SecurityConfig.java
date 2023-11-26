945cbda8caec467895593348fe2cf6a5457334af
H33Kz <qlmejk@gmail.com>
2023-11-26 10:04:29 +0100

    Csrf disabled for developement

--- src/main/java/com/h33kz/WarehouseManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.h33kz.WarehouseManagementSystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,10 +25,10 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     return http.authorizeHttpRequests(auth -> auth
-      // .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
-      // .requestMatchers(HttpMethod.GET).hasAuthority("USER")
-      // .anyRequest().denyAll())
-      .anyRequest().permitAll())
+      .requestMatchers(HttpMethod.POST).hasAuthority("ADMIN")
+      .requestMatchers(HttpMethod.GET).hasAuthority("USER")
+      .anyRequest().denyAll())
+      // .anyRequest().permitAll())
       .csrf(csrf -> csrf.disable())
       .httpBasic(Customizer.withDefaults())
       .userDetailsService(jpaUserDetailsService)
