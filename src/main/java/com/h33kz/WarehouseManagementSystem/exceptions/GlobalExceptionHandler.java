a6806f9cb400a0618fa6457bc3b3532dda0cc836
H33Kz <qlmejk@gmail.com>
2023-11-26 17:00:29 +0100

    Global exception handler for handling all custom exceptions

---
+++ src/main/java/com/h33kz/WarehouseManagementSystem/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.h33kz.WarehouseManagementSystem.exceptions;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(value = {ProductNotFoundException.class,UserNotFoundException.class})
+  public ResponseEntity<Object> handleResourceNotFoundException(Exception e){
+    ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z")));
+    return new ResponseEntity<>(exceptionResponse,HttpStatus.NOT_FOUND);
+  }
+  
+  @ExceptionHandler(value = {PriceLowerThanZeroException.class, AmountLowerThanZeroException.class})
+  public ResponseEntity<Object> handleInproperRequestValueException(Exception e){
+    ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(), HttpStatus.CONFLICT, ZonedDateTime.now(ZoneId.of("Z")));
+    return new ResponseEntity<>(exceptionResponse,HttpStatus.CONFLICT);
+  }
+}
