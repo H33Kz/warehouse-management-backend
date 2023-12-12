fe414493234aad07d66aa63161fee2cacc501248
H33Kz <qlmejk@gmail.com>
2023-12-12 09:07:37 +0100

    Customer controller created
    Controlelr for routing customer info manipulation and getting resources regarding them

--- src/main/java/com/h33kz/WarehouseManagementSystem/controllers/CustomerController.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/controllers/CustomerController.java
@@ -1,21 +1,47 @@
 package com.h33kz.WarehouseManagementSystem.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.h33kz.WarehouseManagementSystem.models.Customer;
 import com.h33kz.WarehouseManagementSystem.service.CustomerService;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
   @Autowired
   private CustomerService customerService;
 
-  @PostMapping
+  @PostMapping("/insert")
   public Customer insertCustomer(@RequestBody Customer customer){
     return customerService.insertCustomer(customer);
+  }
+
+  @GetMapping("/remove/{id}")
+  public Customer removeCustomer(@PathVariable int id){
+    return customerService.removeCustomer(id);
+  }
+
+  @GetMapping("/getWith/name/{searchString}")
+  public List<Customer> getCustomersWithName(@PathVariable String searchString){
+    return customerService.getCustomersWithName(searchString);
+  }
+
+  @GetMapping("/update/name/{id}/{newName}")
+  public Customer updateCustomerNameById(@PathVariable(name = "id") int id, @PathVariable(name = "newName") String newName){
+    return customerService.updateCustomerNameById(id, newName);
+  }
+
+  @GetMapping("/update/eg/{id}/{newEg}")
+  public Customer updateCustomerEGById(@PathVariable(name = "id") int id, @PathVariable(name = "newEg") long newEg){
+    return customerService.updateCustomerEGById(id, newEg);
   }
   
 }
