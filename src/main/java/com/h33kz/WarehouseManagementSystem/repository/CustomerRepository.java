a63d7d91f7462626e6eea18644aab6082325c2c5
H33Kz <qlmejk@gmail.com>
2023-12-12 09:08:03 +0100

    Customer repository created

--- src/main/java/com/h33kz/WarehouseManagementSystem/repository/CustomerRepository.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.h33kz.WarehouseManagementSystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.h33kz.WarehouseManagementSystem.models.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+  List<Customer> findByNameContaining(String infix);
+  List<Customer> findByCompanyEg(long eg);
 }
