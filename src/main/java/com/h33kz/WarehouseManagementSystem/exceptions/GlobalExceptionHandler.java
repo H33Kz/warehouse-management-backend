3b460e53387e30f86f707a185e825e48ab73c1a2
H33Kz <qlmejk@gmail.com>
2023-12-12 09:07:00 +0100

    Exception regarding customer service functionality

--- src/main/java/com/h33kz/WarehouseManagementSystem/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/exceptions/GlobalExceptionHandler.java
@@ -11,13 +11,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-  @ExceptionHandler(value = {ProductNotFoundException.class,UserNotFoundException.class})
+  @ExceptionHandler(value = {ProductNotFoundException.class,UserNotFoundException.class,CustomerNotFoundException.class})
   public ResponseEntity<Object> handleResourceNotFoundException(Exception e){
     ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now(ZoneId.of("Z")));
     return new ResponseEntity<>(exceptionResponse,HttpStatus.NOT_FOUND);
   }
   
-  @ExceptionHandler(value = {PriceLowerThanZeroException.class, AmountLowerThanZeroException.class, UserAlreadyExistsException.class})
+  @ExceptionHandler(value = {
+    PriceLowerThanZeroException.class, 
+    AmountLowerThanZeroException.class, 
+    UserAlreadyExistsException.class, 
+    CustomerWithThatEGExistsException.class
+  })
   public ResponseEntity<Object> handleInproperRequestValueException(Exception e){
     ExceptionResponse exceptionResponse = new ExceptionResponse(e.getMessage(), HttpStatus.CONFLICT, ZonedDateTime.now(ZoneId.of("Z")));
     return new ResponseEntity<>(exceptionResponse,HttpStatus.CONFLICT);
