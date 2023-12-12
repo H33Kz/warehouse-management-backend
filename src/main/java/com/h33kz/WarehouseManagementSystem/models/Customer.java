a489d9aeb60b7843614e11992d61be0f0bc95852
H33Kz <qlmejk@gmail.com>
2023-12-12 09:05:12 +0100

    Customer model modified for unique column

--- src/main/java/com/h33kz/WarehouseManagementSystem/models/Customer.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/models/Customer.java
@@ -1,5 +1,6 @@
 package com.h33kz.WarehouseManagementSystem.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,5 +23,6 @@ public class Customer {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   private String name;
+  @Column(unique = true)
   private long companyEg;
 }
