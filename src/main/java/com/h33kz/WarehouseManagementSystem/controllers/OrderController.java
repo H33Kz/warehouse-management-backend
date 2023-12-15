51d582184bbcdcaa1a89b792c08debfa6afb7971
H33Kz <qlmejk@gmail.com>
2023-12-15 17:13:28 +0100

    Added routes for service methods

--- src/main/java/com/h33kz/WarehouseManagementSystem/controllers/OrderController.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/controllers/OrderController.java
@@ -1,7 +1,6 @@
 package com.h33kz.WarehouseManagementSystem.controllers;
 
-import java.awt.List;
-import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.h33kz.WarehouseManagementSystem.models.Customer;
 import com.h33kz.WarehouseManagementSystem.models.Order;
-import com.h33kz.WarehouseManagementSystem.models.OrderedItem;
-import com.h33kz.WarehouseManagementSystem.models.Product;
 import com.h33kz.WarehouseManagementSystem.service.OrderService;
 
 @RestController
@@ -26,36 +22,26 @@ public class OrderController {
   
   @PostMapping("/insert")
   public Order insertOrder(@RequestBody Order order){
-    // Product mockProduct =new Product();
-    // Customer mockCustomer = new Customer();
-    // OrderedItem mockOrderItem = new OrderedItem();
-    // mockProduct.setId(1);
-    // mockCustomer.setId(1);
-    // mockOrderItem.setQuantity(1);
-    // mockOrderItem.setProduct(mockProduct);
-    //
-    // Order mockOrder = new Order();
-    // mockOrder.setCustomer(mockCustomer);
-    //
-    // LinkedList<OrderedItem> mockOrderedItems = new LinkedList<>();
-    // mockOrderedItems.add(mockOrderItem);
-    //
-    // mockProduct =new Product();
-    // mockOrderItem = new OrderedItem();
-    // mockProduct.setId(2);
-    // mockOrderItem.setQuantity(1);
-    // mockOrderItem.setProduct(mockProduct);
-    //
-    // mockOrderedItems.add(mockOrderItem);
-    //
-    // mockOrder.setOrderedItems(mockOrderedItems);
-    // return orderService.insertOrder(mockOrder);
     return orderService.insertOrder(order);
   }
 
-  @GetMapping("/getWith/{id}")
+  @GetMapping("/remove/{id}")
+  public Order removeOrder(@PathVariable int id){
+    return orderService.removeOrder(id);
+  }
+
+  @GetMapping("/get/{id}")
   public Order getOrderWithId(@PathVariable int id){
     return orderService.getOrderById(id);
   }
-  
+ 
+  @GetMapping("/getOrdered/id")
+  public List<Order> getAllById(){
+    return orderService.getAllById();
+  }
+
+  @GetMapping("/getWith/{customerId}")
+  public List<Order> getAllWithCustomer(@PathVariable int customerId){
+    return orderService.getALlWithCustomer(customerId);
+  }
 }
