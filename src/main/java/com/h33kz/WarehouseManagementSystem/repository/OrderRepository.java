3454a2db9b5d48cf9ed22cdc4a68517a80b5d119
H33Kz <qlmejk@gmail.com>
2023-12-15 16:36:07 +0100

    Added finding orders placed for specific customer

--- src/main/java/com/h33kz/WarehouseManagementSystem/repository/OrderRepository.java
+++ src/main/java/com/h33kz/WarehouseManagementSystem/repository/OrderRepository.java
@@ -1,10 +1,14 @@
 package com.h33kz.WarehouseManagementSystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.h33kz.WarehouseManagementSystem.models.Customer;
 import com.h33kz.WarehouseManagementSystem.models.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Integer>{  
+public interface OrderRepository extends JpaRepository<Order,Integer>{
+  List<Order> findByCustomer(Customer customer);
 }
